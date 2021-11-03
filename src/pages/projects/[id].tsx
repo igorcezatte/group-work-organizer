4c3a03d726ad40447e5ee9c72892a4573ef7efe1
YPAzevedo <yagopereira@bitgo.com>
2021-11-02 18:11:51 -0700

    feat: ui improvements

--- src/pages/projects/[id].tsx
+++ src/pages/projects/[id].tsx
@@ -66,7 +66,7 @@ export default function ProjectPage() {
             padding: '0 1rem',
           }}
         >
-          <NavLink href="/projects">
+          <NavLink href="/projects" noUnderline>
             <Button color="secondary" variant="contained">
               Voltar
             </Button>
